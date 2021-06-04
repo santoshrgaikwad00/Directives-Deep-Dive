ad6cdde2584758c56c65b948bcde3adef0fb6099
santoshrgaikwad00 <santoshrgaikwad00@gmail.com>
2021-06-04 01:01:41 -0700

    template created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	
+	onlyOdd : boolean = false;
+
+	oddNumbers = [1,3,5,7,9];
+	evenNumbers = [2,4,6,8];
 }
